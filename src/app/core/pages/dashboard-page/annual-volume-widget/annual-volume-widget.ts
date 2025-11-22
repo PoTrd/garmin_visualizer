07ce7c1ab206c2d9a6e8c2441a23f976b815c882
PoTrd <paul.taurand@gmail.com>
2025-11-22 11:13:17 +0100

    Round bar value

--- src/app/core/pages/dashboard-page/annual-volume-widget/annual-volume-widget.ts
+++ src/app/core/pages/dashboard-page/annual-volume-widget/annual-volume-widget.ts
@@ -16,73 +16,74 @@ type displayedData = {
   styleUrl: './annual-volume-widget.css',
 })
 export class AnnualVolumeWidget {
-  @ViewChildren('animatedBar') bars!: QueryList<ElementRef>;
+    @ViewChildren('animatedBar') bars!: QueryList<ElementRef>;
 
-  constructor(private renderer: Renderer2) {}
+    constructor(private renderer: Renderer2) {}
 
-  triggerAnimation(): void {
+    triggerAnimation(): void {
     this.bars.forEach((bar) => {
-      this.renderer.removeClass(bar.nativeElement, 'animate');
-      void bar.nativeElement.offsetWidth;
-      this.renderer.addClass(bar.nativeElement, 'animate');
+        this.renderer.removeClass(bar.nativeElement, 'animate');
+        void bar.nativeElement.offsetWidth;
+        this.renderer.addClass(bar.nativeElement, 'animate');
     });
-  }
+    }
 
-  data = input<Activity[]>();
-  selectedMonth = output<Months>();
-  filter = input<TypeFilterType>();
-  lstMonths: Months[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  displayedData = computed<displayedData[]>(() => {
-      const activities = this.data() ?? [];
-      const monthsToDisplay = this.getLast12Months();
-      const result: displayedData[] = [];
-      for(let month of monthsToDisplay) {
-          const monthActivities: Activity[] = activities.filter(activity => {
-              const activityMonth = activity.date.toLocaleString('en-US', { month: 'long' }) as Months;
-              return activityMonth === month;
-          });
-          let totalValue = 0;
-          switch(this.filter()) {
-              case 'Distance':
-                  totalValue = monthActivities.reduce((sum, act) => sum + act.distance, 0);
-                  break;
-              case 'Duration':
-                  totalValue = monthActivities.reduce((sum, act) => sum + act.duration, 0);
-                  break;
-              case 'Calories':
-                  totalValue = monthActivities.reduce((sum, act) => sum + act.calories, 0);
-                  break;
-              case 'Ascent':
-                  totalValue = monthActivities.reduce((sum, act) => sum + act.totalAscent, 0);
-                  break;
-          }
-          
-          result.push({
-              month: month,
-              value: totalValue
-          });
-      }
-      const maxValue = Math.max(...result.map(d => d.value));
-      
-      for(let data of result) {
-        data.value = maxValue ? (data.value / maxValue) * 100 : 0;
-      }
+    data = input<Activity[]>();
+    selectedMonth = output<Months>();
+    filter = input<TypeFilterType>();
+    lstMonths: Months[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    displayedData = computed<displayedData[]>(() => {
+        const activities = this.data() ?? [];
+        const monthsToDisplay = this.getLast12Months();
+        const result: displayedData[] = [];
+        for(let month of monthsToDisplay) {
+            const monthActivities: Activity[] = activities.filter(activity => {
+                const activityMonth = activity.date.toLocaleString('en-US', { month: 'long' }) as Months;
+                return activityMonth === month;
+            });
+            let totalValue = 0;
+            switch(this.filter()) {
+                case 'Distance':
+                    totalValue = monthActivities.reduce((sum, act) => sum + act.distance, 0);
+                    break;
+                case 'Duration':
+                    totalValue = monthActivities.reduce((sum, act) => sum + act.duration, 0);
+                    break;
+                case 'Calories':
+                    totalValue = monthActivities.reduce((sum, act) => sum + act.calories, 0);
+                    break;
+                case 'Ascent':
+                    totalValue = monthActivities.reduce((sum, act) => sum + act.totalAscent, 0);
+                    break;
+            }
+            
+            result.push({
+                month: month,
+                value: totalValue
+            });
+        }
+        const maxValue = Math.max(...result.map(d => d.value));
+        
+        for(let data of result) {
+            const scaledValue = (data.value / maxValue) * 100;
+            const roundedScaledValue = Math.round(scaledValue / 10) * 10;
+            data.value = roundedScaledValue;
+        }
 
-      return result;
-  });
+        return result;
+    });
 
-  getLast12Months(): Months[] {
-      const result: Months[] = [];
-      const now = new Date();
-      this.lstMonths.forEach((month, index) => {
-          const monthIndex = (now.getMonth() + 1 - index + 12) % 12;
-          result.unshift(this.lstMonths[monthIndex]);
-      });
-      return result;
-  }
+    getLast12Months(): Months[] {
+        const result: Months[] = [];
+        const now = new Date();
+        this.lstMonths.forEach((month, index) => {
+            const monthIndex = (now.getMonth() + 1 - index + 12) % 12;
+            result.unshift(this.lstMonths[monthIndex]);
+        });
+        return result;
+    }
 
     selectMonth(month: Months): void {
         this.selectedMonth.emit(month);
     }
-
 }
